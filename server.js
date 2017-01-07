0a74c38d453293b7111f98d1aa2bcb51eb50da38
Md. Shahriar Anwar <anwarshahriar@gmail.com>
2017-01-07 08:05:39 +0000

    Fix error for static resource request

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 var express = require('express');
-var url = require('url');
+var URL = require('url');
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(function (req, res, next) {
+    if (req.url.endsWith('/favicon.ico')) res.end(404);
+    next();
+});
+
 app.get('*', function (req, res) {
-    var urlData = url.parse(req.url);
+    var urlData = URL.parse(req.url);
+    
     if (urlData.path.startsWith('/new/')) {
         var url = urlData.path.slice(5, urlData.path.length);
-        generateShortUrl(url, function (err, result) {
+        generateShortUrl(req.headers.host, url, function (err, result) {
             if (err) throw err;
             res.json(result);
         });
     } else {
         var shortUrl = urlData.path.slice(1, urlData.path.length);
-        getOriginalUrl(shortUrl, function (err, originalUrl) {
-            if (err) res.json({ error: "This url is not on the database." });
+        getOriginalUrl(req.headers.host, shortUrl, function (err, originalUrl) {
+            if (err) return res.json({ error: "This url is not on the database." });
             redirectTo(res, originalUrl);
         });
     }
@@ -26,17 +32,18 @@ app.listen(PORT, function () {
 });
 
 var data = [];
-function generateShortUrl(url, callback) {
+function generateShortUrl(host, url, callback) {
     var result = {
         original_url: url,
-        short_url: Date.now()
+        short_url: 'https://' + host + '/' + Date.now()
     };
     data.push(result);
     
     callback(null, result);
 }
 
-function getOriginalUrl(shortUrl, callback) {
+function getOriginalUrl(host, shortUrl, callback) {
+    shortUrl = 'https://' + host + '/' + shortUrl;
     for (var i = 0; i < data.length; i++) {
         if (data[i].short_url === shortUrl) {
             return callback(null, data[i].original_url);
